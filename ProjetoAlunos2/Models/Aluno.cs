cf58b2ea1a9f662aba69d75949523bb6b3f312f4
Fernando Lima <fernando.ilustrar@gmail.com>
2021-09-30 19:03:40 -0300

    Criacao do projeto; Criacao do Model Aluno

---
+++ ProjetoAlunos2/Models/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAlunos2.Models
+{
+    public class Aluno
+    {
+
+        [Display(Name = "Matrícula")]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        [Display(Name = "Data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DataDeNascimento { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public Aluno(int id, string nome, string sobrenome, DateTime dataDeNascimento, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataDeNascimento = dataDeNascimento;
+            Email = email;
+        }
+    }
+}
