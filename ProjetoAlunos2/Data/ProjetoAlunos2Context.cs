bee1410a2e253ffba6dbe3f1845b4e8f0f14bd3d
Fernando Lima <fernando.ilustrar@gmail.com>
2021-09-30 19:09:01 -0300

    Scaffold de AlunosController com EF

---
+++ ProjetoAlunos2/Data/ProjetoAlunos2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoAlunos2.Models;
+
+namespace ProjetoAlunos2.Data
+{
+    public class ProjetoAlunos2Context : DbContext
+    {
+        public ProjetoAlunos2Context (DbContextOptions<ProjetoAlunos2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoAlunos2.Models.Aluno> Aluno { get; set; }
+    }
+}
--- ProjetoAlunos2/Startup.cs
+++ ProjetoAlunos2/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoAlunos2.Data;
 
 namespace ProjetoAlunos2
 {
@@ -33,6 +35,9 @@ namespace ProjetoAlunos2
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoAlunos2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoAlunos2Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
