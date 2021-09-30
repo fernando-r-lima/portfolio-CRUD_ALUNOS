e71a2e713714fbf78391a12acc5311cfb75d20b4
Fernando Lima <fernando.ilustrar@gmail.com>
2021-09-30 19:47:05 -0300

    SeedingService para popular DB no desenvolvimento

---
+++ ProjetoAlunos2/Data/SeedingService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoAlunos2.Models;
+
+namespace ProjetoAlunos2.Data
+{
+    public class SeedingService
+    {
+
+        private ProjetoAlunos2Context _context;
+
+        public SeedingService(ProjetoAlunos2Context context)
+        {
+            _context = context;
+        }
+
+        public void PopularDb()
+        {
+            if (_context.Aluno.Any())
+            {
+                return; //Db já populado
+            }
+
+            Aluno a1 = new Aluno(1, "Fernando", "Azevedo", new DateTime(1995, 6, 10), "fernando.Az@gmail.com");
+            Aluno a2 = new Aluno(2, "Luiz Roberto", "Gouveia", new DateTime(1995, 8, 12), "lr.gouveia@uol.com");
+            Aluno a3 = new Aluno(3, "Juliana", "Silva", new DateTime(1994, 2, 9), "ju_silva94@yahoo.com");
+            Aluno a4 = new Aluno(4, "Felipe", "Carvalho", new DateTime(1995, 1, 24), "felipe-carvalho@gmail.com");
+            Aluno a5 = new Aluno(5, "Amanda", "Lima", new DateTime(1993, 4, 15), "mandy-lima@yahoo.com");
+            Aluno a6 = new Aluno(6, "Luiz Augusto", "Alcântara", new DateTime(1993, 5, 8), "luizAlc@gmail.com");
+
+            _context.Aluno.AddRange(a1, a2, a3, a4, a5, a6);
+
+            _context.SaveChanges();
+        }
+    }
+}
--- ProjetoAlunos2/Startup.cs
+++ ProjetoAlunos2/Startup.cs
@@ -39,14 +39,17 @@ namespace ProjetoAlunos2
             services.AddDbContext<ProjetoAlunos2Context>(options =>
                     options.UseMySql(Configuration.GetConnectionString("ProjetoAlunos2Context"), builder =>
                     builder.MigrationsAssembly("ProjetoAlunos2")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.PopularDb();
             }
             else
             {
